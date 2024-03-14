c32acc2a0e0ac5878d4c2f8f52eee5636834a8b7
alvaroquintero28 <162182742+alvaroquintero28@users.noreply.github.com>
2024-03-14 14:22:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import plotly.express as px
+from shiny.express import input, ui
+from shinywidgets import render_plotly
+import palmerpenguins  # this package provides the Palmer Penguins dataset
 
+# use the built in function to load the Palmer Penguins dataset
+penguins_df = palmerpenguins.load_penguins()
+
+ui.page_opts(title="Alvaro's King Penguin data", fillable=True)
+with ui.layout_columns():
+
+    @render_plotly
+    def plot1():
+        return px.histogram(px.data.tips(), y="tip")
+
+    @render_plotly
+    def plot2():
+        return px.histogram(px.data.tips(), y="total_bill")
